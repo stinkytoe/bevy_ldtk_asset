1afb244a8c106aa84d4094752abbc2575b778a47
stinkytoe <stinkytoe42@yahoo.com>
2024-03-25 19:59:10 -0500

    initial addition of rapier2d to side_scroller

--- examples/side_scroller.rs
+++ examples/side_scroller.rs
@@ -1,6 +1,7 @@
 use bevy::{log::LogPlugin, prelude::*};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_ldtk_asset::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 fn main() {
     App::new()
@@ -8,16 +9,18 @@ fn main() {
             DefaultPlugins
                 .set(LogPlugin {
                     level: bevy::log::Level::WARN,
-                    filter: "bevy_ldtk_asset=debug,top_down=debug".into(),
+                    filter: "bevy_ldtk_asset=debug,side_scroller=debug".into(),
                     ..default()
                 })
                 .set(ImagePlugin::default_nearest()),
             WorldInspectorPlugin::default(),
+            RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(64.0),
+            RapierDebugRenderPlugin::default(),
             BevyLdtkAssetPlugin,
         ))
         .insert_resource(Msaa::Off)
         .add_systems(Startup, setup)
-        // .add_systems(Update, move_player)
+        .add_systems(Update, added_ldtk_entities)
         .run();
 }
 
@@ -36,4 +39,39 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         spawn_entities: SpawnEntities::Everything,
         ..default()
     });
+}
+
+fn added_ldtk_entities(
+    mut commands: Commands,
+    added_ldtk_entities_query: Query<(Entity, &LdtkEntity), Added<LdtkEntity>>,
+) {
+    for (entity, ldtk_entity) in added_ldtk_entities_query.iter() {
+        if ldtk_entity.has_tag("player") {
+            info!("Adding KinematicCharacterController to entity: {entity:?}");
+            commands.entity(entity).insert((
+                // Collider::cuboid(ldtk_entity.size().x / 2.0, ldtk_entity.size().y / 2.0),
+                KinematicCharacterController {
+                    // translation: Some(ldtk_entity.pivot() * ldtk_entity.size() * Vec2::splat(0.5)),
+                    custom_shape: Some((
+                        Collider::cuboid(ldtk_entity.size().x / 2.0, ldtk_entity.size().y / 2.0),
+                        (ldtk_entity.pivot() * ldtk_entity.size() * Vec2::splat(0.5)),
+                        // ldtk_entity.pivot(),
+                        0.0,
+                    )),
+                    // custom_mass: todo!(),
+                    up: Vec2::Y,
+                    // offset: todo!(),
+                    // slide: todo!(),
+                    // autostep: todo!(),
+                    // max_slope_climb_angle: todo!(),
+                    // min_slope_slide_angle: todo!(),
+                    // apply_impulse_to_dynamic_bodies: todo!(),
+                    // snap_to_ground: todo!(),
+                    // filter_flags: todo!(),
+                    // filter_groups: todo!(),
+                    ..default()
+                },
+            ));
+        }
+    }
 }
