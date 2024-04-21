3fc587be3722468db629da89b8b7c3f7905ebd5a
stinkytoe <stinkytoe42@yahoo.com>
2024-04-21 16:11:51 -0500

    further improvemenst to LevelBundleLoadSettings

--- src/level/bundle.rs
+++ src/level/bundle.rs
@@ -14,6 +14,14 @@ pub enum LoadLayers {
 
 #[derive(Clone, Debug, Default)]
 #[cfg_attr(feature = "enable_reflect", derive(Reflect))]
+pub enum LoadLayerMode {
+    ComponentOnly,
+    #[default]
+    Mesh,
+}
+
+#[derive(Clone, Debug, Default)]
+#[cfg_attr(feature = "enable_reflect", derive(Reflect))]
 pub enum LoadEntities {
     None,
     ByIdentifiers(Vec<String>),
@@ -31,12 +39,28 @@ pub enum LoadEntityMode {
     Sprite,
 }
 
-#[derive(Clone, Component, Debug, Default)]
+#[derive(Clone, Component, Debug)]
 #[cfg_attr(feature = "enable_reflect", derive(Reflect))]
 pub struct LevelBundleLoadSettings {
+    pub load_bg_color: bool,
+    pub load_bg_image: bool,
     pub load_layers: LoadLayers,
+    pub load_layer_mode: LoadLayerMode,
     pub load_entities: LoadEntities,
     pub load_entity_mode: LoadEntityMode,
+}
+
+impl Default for LevelBundleLoadSettings {
+    fn default() -> Self {
+        Self {
+            load_bg_color: true,
+            load_bg_image: true,
+            load_layers: Default::default(),
+            load_layer_mode: Default::default(),
+            load_entities: Default::default(),
+            load_entity_mode: Default::default(),
+        }
+    }
 }
 
 #[derive(Bundle, Debug, Default)]
