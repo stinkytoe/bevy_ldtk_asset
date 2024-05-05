89b95d366694dcb502d9cd34f6a56ef446a1739a
Randal Robinson <stinkytoe42@yahoo.com>
2024-05-05 16:00:11 -0500

    extended util function to indlude all Anchor variants

--- src/util.rs
+++ src/util.rs
@@ -61,8 +61,17 @@ pub(crate) fn bevy_anchor_from_ldtk(pivot: &[f64]) -> Result<Anchor, AnchorIntoE
         return Err(AnchorIntoError::BadArrayLength);
     }
 
-    Ok(Anchor::Custom(Vec2::new(
-        pivot[0] as f32 - 0.5,
-        0.5 - pivot[1] as f32,
-    )))
+    #[allow(illegal_floating_point_literal_pattern)]
+    Ok(match (pivot[0] as f32, pivot[1] as f32) {
+        (0.0, 0.0) => Anchor::TopLeft,
+        (0.0, 0.5) => Anchor::CenterLeft,
+        (0.0, 1.0) => Anchor::BottomLeft,
+        (0.5, 0.0) => Anchor::TopCenter,
+        (0.5, 0.5) => Anchor::Center,
+        (0.5, 1.0) => Anchor::BottomCenter,
+        (1.0, 0.0) => Anchor::TopRight,
+        (1.0, 0.5) => Anchor::CenterRight,
+        (1.0, 1.0) => Anchor::BottomRight,
+        (x, y) => Anchor::Custom(Vec2::new(x - 0.5, 0.5 - y)),
+    })
 }
