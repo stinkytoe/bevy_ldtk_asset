49cd9ab47deec8029381c081791935dc03ba6f9b
Randal Robinson <stinkytoe42@yahoo.com>
2024-05-29 08:03:56 -0500

    merged add and modify for WorldAsset systems

--- src/world/systems.rs
+++ src/world/systems.rs
@@ -9,53 +9,56 @@ use super::WorldAsset;
 pub enum NewWorldAssetSystemError {
     #[error("Bad world handle!")]
     BadWorldHandle,
-    // #[error("Bad project asset path!")]
-    // BadProjectAssetPath,
 }
 
+#[allow(clippy::type_complexity)]
 pub(crate) fn new_world_asset(
     mut events: EventReader<AssetEvent<WorldAsset>>,
     mut commands: Commands,
-    new_world_query: Query<(Entity, &Handle<WorldAsset>)>,
-    mut modified_world_query: Query<(&Handle<WorldAsset>, &mut Iid)>,
+    mut modified_world_query: Query<(
+        Entity,
+        &Handle<WorldAsset>,
+        Option<&mut Name>,
+        Option<&mut Iid>,
+    )>,
     removed_world_query: Query<(Entity, &Handle<WorldAsset>)>,
     world_assets: Res<Assets<WorldAsset>>,
 ) -> Result<(), NewWorldAssetSystemError> {
     for event in events.read() {
         match event {
             AssetEvent::Added { id } | AssetEvent::LoadedWithDependencies { id } => {
-                for (entity, world_handle) in new_world_query
-                    .iter()
-                    .filter(|(_, handle)| handle.id() == *id)
-                {
-                    let world_asset = world_assets
-                        .get(*id)
-                        .ok_or(NewWorldAssetSystemError::BadWorldHandle)?;
-
-                    debug!("Adding components for: {:?}", world_handle.path());
-
-                    commands.entity(entity).insert((
-                        Name::from(world_asset.identifier.clone()),
-                        Iid {
-                            iid: world_asset.iid.clone(),
-                        },
-                    ));
-                }
-            }
-            AssetEvent::Modified { id } => {
-                for (world_handle, mut iid) in modified_world_query
+                for (entity, world_handle, name, iid) in modified_world_query
                     .iter_mut()
-                    .filter(|(handle, _)| handle.id() == *id)
+                    .filter(|(_, handle, _, _)| handle.id() == *id)
                 {
                     let project_asset = world_assets
                         .get(*id)
                         .ok_or(NewWorldAssetSystemError::BadWorldHandle)?;
 
-                    debug!("Modifying components for: {:?}", world_handle.path());
+                    let world_asset = world_assets
+                        .get(*id)
+                        .ok_or(NewWorldAssetSystemError::BadWorldHandle)?;
 
-                    iid.iid.clone_from(&project_asset.iid);
+                    debug!("Adding/Modifying components for: {:?}", world_handle.path());
+
+                    if let Some(mut name) = name {
+                        name.set(world_asset.identifier.clone());
+                    } else {
+                        commands
+                            .entity(entity)
+                            .insert(Name::from(world_asset.identifier.clone()));
+                    };
+
+                    if let Some(mut iid) = iid {
+                        iid.iid.clone_from(&project_asset.iid);
+                    } else {
+                        commands.entity(entity).insert(Iid {
+                            iid: world_asset.iid.clone(),
+                        });
+                    };
                 }
             }
+            AssetEvent::Modified { id: _ } => {}
             AssetEvent::Removed { id } | AssetEvent::Unused { id } => {
                 for (entity, _) in removed_world_query
                     .iter()
