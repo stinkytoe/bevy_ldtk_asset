ecb3df3eac604a2574d6c16a8577b415907a9283
stinkytoe <stinkytoe42@yahoo.com>
2024-09-27 00:32:42 -0500

    experiment with headless mode for all_features example

--- examples/all_features.rs
+++ examples/all_features.rs
@@ -1,11 +1,25 @@
-use bevy::prelude::*;
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use std::time::Duration;
+
+use bevy::{
+    app::ScheduleRunnerPlugin,
+    log::{Level, LogPlugin},
+    prelude::*,
+};
 
 fn main() {
     App::new()
         .add_plugins((
-            DefaultPlugins,
-            WorldInspectorPlugin::default(),
+            AssetPlugin::default(),
+            ImagePlugin::default(),
+            LogPlugin {
+                level: Level::WARN,
+                filter: "bevy_ldtk_asset=trace,example=trace".into(),
+                ..default()
+            },
+            MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(Duration::from_secs_f32(
+                1.0 / 60.0,
+            ))),
+            //WorldInspectorPlugin::default(),
             bevy_ldtk_asset::plugin::BevyLdtkAssetPlugin,
         ))
         .add_systems(Startup, startup)
@@ -13,7 +27,7 @@ fn main() {
 }
 
 fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
+    //commands.spawn(Camera2dBundle::default());
 
     commands
         .spawn(asset_server.load::<bevy_ldtk_asset::project::Project>("ldtk/all_features.ldtk"));
