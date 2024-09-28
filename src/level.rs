068cd65eda00727fc74f146b2c245337daa4e2ea
stinkytoe <stinkytoe42@yahoo.com>
2024-09-27 19:12:53 -0500

    added prelude, reworked some visibility

--- src/level.rs
+++ src/level.rs
@@ -11,8 +11,57 @@ use crate::field_instance::FieldInstance;
 use crate::iid::Iid;
 use crate::ldtk;
 use crate::ldtk_asset::LdtkAsset;
-use crate::neighbour::Neighbour;
 
+#[derive(Debug, Reflect)]
+pub enum NeighbourDir {
+    North,
+    South,
+    East,
+    West,
+    Lower,
+    Greater,
+    Overlap,
+    NorthWest,
+    NorthEast,
+    SouthWest,
+    SouthEast,
+}
+
+impl NeighbourDir {
+    fn new(dir: &str) -> Result<Self, Error> {
+        match dir {
+            "n" => Ok(Self::North),
+            "s" => Ok(Self::South),
+            "w" => Ok(Self::West),
+            "e" => Ok(Self::East),
+            "<" => Ok(Self::Lower),
+            ">" => Ok(Self::Greater),
+            "o" => Ok(Self::Overlap),
+            "nw" => Ok(Self::NorthWest),
+            "ne" => Ok(Self::NorthEast),
+            "sw" => Ok(Self::SouthWest),
+            "se" => Ok(Self::SouthEast),
+            _ => Err(Error::LdtkImportError(format!(
+                "Bad direction from LDtk neighbor! given: {dir}"
+            ))),
+        }
+    }
+}
+
+#[derive(Debug, Reflect)]
+pub struct Neighbour {
+    pub dir: NeighbourDir,
+    pub level_iid: Iid,
+}
+
+impl Neighbour {
+    pub(crate) fn new(value: &ldtk::NeighbourLevel) -> Result<Self, Error> {
+        let dir = NeighbourDir::new(&value.dir)?;
+        let level_iid = Iid::from_str(&value.level_iid)?;
+
+        Ok(Self { dir, level_iid })
+    }
+}
 #[derive(Debug, Reflect)]
 pub struct LevelBackgroundPosition {
     pub crop_rect: Rect,
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ mod anchor;
 mod color;
 mod ldtk;
 mod ldtk_path;
+mod project_loader;
 
 pub mod entity;
 pub mod enum_definition;
@@ -13,13 +14,26 @@ pub mod iid;
 pub mod layer;
 pub mod ldtk_asset;
 pub mod level;
-pub mod neighbour;
 pub mod plugin;
 pub mod project;
-pub mod project_loader;
 pub mod tile_instance;
 pub mod tileset_definition;
 pub mod tileset_rectangle;
 pub mod world;
 
-pub mod prelude {}
+pub mod prelude {
+    pub use crate::entity::Entity;
+    pub use crate::enum_definition::{EnumDefinition, EnumValueDefinition};
+    pub use crate::error::Error;
+    pub use crate::field_instance::{EntityRef, FieldInstance, FieldInstanceType};
+    pub use crate::iid::{Iid, IidError, IidMap, IidSet};
+    pub use crate::layer::{EntitiesLayer, Layer, LayerType, TilesLayer};
+    pub use crate::ldtk_asset::LdtkAsset;
+    pub use crate::level::{Level, LevelBackgroundPosition, Neighbour, NeighbourDir};
+    pub use crate::plugin::BevyLdtkAssetPlugin;
+    pub use crate::project::Project;
+    pub use crate::tile_instance::TileInstance;
+    pub use crate::tileset_definition::{EnumTagValue, TileCustomMetadata, TilesetDefinition};
+    pub use crate::tileset_rectangle::TilesetRectangle;
+    pub use crate::world::World;
+}
