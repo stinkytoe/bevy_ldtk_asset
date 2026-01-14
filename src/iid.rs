ee1b2ad7ec8b52a8913c410150f899d9be0a56bb
stinkytoe <stinkytoe42@yahoo.com>
2026-01-13 19:34:04 -0600

    removed tests from iid, relying on the uuid crate for correctness

--- src/iid.rs
+++ src/iid.rs
@@ -25,28 +25,3 @@ pub type IidMap<T> = HashMap<Iid, T>;
 
 /// A [bevy_utils::HashSet] of [Iid]s, for convenience.
 pub type IidSet = HashSet<Iid>;
-
-mod test {
-    #[test]
-    #[allow(clippy::unwrap_used)]
-    fn test_from_str() {
-        use super::Iid;
-        use std::str::FromStr;
-
-        let x = Iid::from_str("e41ad760-25d0-11ef-bd94-e3313b674128").unwrap();
-        assert_eq!(x.as_u128(), 0xe41ad76025d011efbd94e3313b674128);
-
-        let x = Iid::from_str("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
-        assert!(x.is_err());
-    }
-
-    #[test]
-    #[allow(clippy::unwrap_used)]
-    fn test_to_string() {
-        use super::Iid;
-        use std::str::FromStr;
-
-        let x = Iid::from_str("e41ad760-25d0-11ef-bd94-e3313b674128").unwrap();
-        assert_eq!(x.to_string(), "e41ad760-25d0-11ef-bd94-e3313b674128");
-    }
-}
